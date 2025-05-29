cf4d66415276583ee08f1d531f1f931f798b47a3
rodrigosainz94 <sainzrodrigomaximiliano@gmail.com>
2025-05-29 18:49:07 -0300

    agrego combo box

--- PruebaGit/Form1.Designer.cs
+++ PruebaGit/Form1.Designer.cs
@@ -32,25 +32,24 @@
             button2 = new Button();
             dataGridView1 = new DataGridView();
             button3 = new Button();
+            comboBox1 = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(138, 479);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(121, 359);
             button1.Name = "button1";
-            button1.Size = new Size(86, 31);
+            button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(357, 364);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(312, 273);
             button2.Name = "button2";
-            button2.Size = new Size(360, 220);
+            button2.Size = new Size(315, 165);
             button2.TabIndex = 1;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
@@ -58,32 +57,40 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(138, 16);
-            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Location = new Point(121, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(418, 304);
+            dataGridView1.Size = new Size(366, 228);
             dataGridView1.TabIndex = 2;
             // 
             // button3
             // 
-            button3.Location = new Point(731, 125);
+            button3.Location = new Point(640, 94);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(94, 29);
+            button3.Size = new Size(82, 22);
             button3.TabIndex = 3;
             button3.Text = "button3";
             button3.UseVisualStyleBackColor = true;
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(586, 189);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 4;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
             Controls.Add(button3);
             Controls.Add(dataGridView1);
             Controls.Add(button2);
             Controls.Add(button1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -96,5 +103,6 @@
         private Button button2;
         private DataGridView dataGridView1;
         private Button button3;
+        private ComboBox comboBox1;
     }
 }
